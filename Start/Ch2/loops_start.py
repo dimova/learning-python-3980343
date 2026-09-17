6cc086ab484e76efe2ae65928890aa5c42f90342
Desi <dimova@gmail.com>
2026-09-17 14:41:02 +0000

    loops

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -5,15 +5,28 @@
 x = 0
 
 # define a while loop
-
+while x < 10:
+    print(x)
+    x += 1
 
 # define a for loop
+for i in range(10):
+    print(i)
 
-
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 # use a for loop over a collection
+for day in days:
+    print(day)
 
 
 # use the break and continue statements
-
-
+for day in days:
+    print(day)
+    if day == "Wednesday":
+        break
+    if day == "Tuesday":
+        continue
+    print("This will not print for Tuesday")
 # using the enumerate() function to get an index and an item
+for index, day in enumerate(days):
+    print(index,day)
