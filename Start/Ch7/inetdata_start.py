66ff7fee690dc752911377900d816dbb13530ddc
Desi <dimova@gmail.com>
2026-09-23 15:02:37 +0000

    read from the internet

--- Start/Ch7/inetdata_start.py
+++ Start/Ch7/inetdata_start.py
@@ -1,3 +1,9 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for retrieving data from the internet
 #
+import urllib.request
+
+url = "http://www.example.com"
+response = urllib.request.urlopen(url)
+data = response.read()
+print(data)
