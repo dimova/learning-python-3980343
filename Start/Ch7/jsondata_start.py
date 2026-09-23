7c4eecd9d45264d7c046cb7dc3b39656c76862b6
Desi <dimova@gmail.com>
2026-09-23 15:46:09 +0000

    json

--- Start/Ch7/jsondata_start.py
+++ Start/Ch7/jsondata_start.py
@@ -3,12 +3,16 @@
 #
 
 import urllib.request 
-
+import json
 
 # Open the URL and read the data
-
-
+web_url = urllib.request.urlopen("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+print("Result code:", web_url.getcode())
 # Read the JSON data from the source
+
+data = web_url.read()
+data = json.loads(data)
 
 
 # Print the content of the 'text' field
+print(data['text'])
