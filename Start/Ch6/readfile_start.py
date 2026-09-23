00c142cc965ad1287ff27cc41754c1f2c22802b1
Desi <dimova@gmail.com>
2026-09-23 14:27:30 +0000

    read files

--- Start/Ch6/readfile_start.py
+++ Start/Ch6/readfile_start.py
@@ -2,7 +2,15 @@
 # Read and write files using the built-in Python file methods
 # LinkedIn Learning Python course by Joe Marini
 #
-
+sample_file = open("sample.txt", "r")
+if sample_file.mode == "r":
+    # contents = sample_file.read()
+    # print(contents)
+    # sample_file.close()
+    file_contents = sample_file.readlines()
+    for line in file_contents:
+        print(line)
+    sample_file.close()
     
 # Open the file and read the contents
 
