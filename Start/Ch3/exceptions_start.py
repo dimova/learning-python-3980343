c7136b4f4204e9e4e081f0c8d8f0334d4fe15bd9
Desi <dimova@gmail.com>
2026-09-23 10:19:15 +0000

    exceptions

--- Start/Ch3/exceptions_start.py
+++ Start/Ch3/exceptions_start.py
@@ -1,10 +1,22 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with Exceptions
 #
-
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+finally:
+    print("This block always executes, regardless of whether an exception occurred.")
 # Errors can happen in programs, and we need a clean way to handle them
 # This code will cause an error because you can't divide by zero:
-
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("A value error occurred.")
+finally:
+    print("This block always executes, regardless of whether an exception occurred.")
 # Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
 
