4c3b9a3c81414190763b239241b3b342b84ea76d
Desi <dimova@gmail.com>
2026-09-23 10:15:33 +0000

    tabulate

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -1,28 +1,37 @@
 # LinkedIn Learning Python course by Joe Marini
 # Working with modules of code
-
+import math
+print(math.sqrt(16))
 # import the math module, which contains features for working with mathematics
 
 
 # import a specific part of the module so you can refer to it more easily
+from math import sqrt
+print(sqrt(16))
 
 
 # import a module and give it a different name
+import math as m
+print(m.sqrt(16))
 
 
 # the math module contains lots of pre-built functions
+print(math.factorial(5))  # Example usage of another math function
 
 
 # in addition to functions, some modules contain useful constants 
+print(math.pi)  # Example usage of a constant from the math module
 
 
 # Generate a random number between 100 and 200
+import random
+print(random.randint(100, 200))
 
 
 # try some of the math functions for yourself here:
 
 # Use the 3rd party tabulate module to print tabulated data:
-
+from tabulate import tabulate
 # Sample data
 data = [
   ["Product", "Price", "Stock"],
@@ -32,3 +41,5 @@ data = [
 ]
 
 # Create a formatted table
+table = tabulate(data, headers="firstrow", tablefmt="grid")
+print(table)
