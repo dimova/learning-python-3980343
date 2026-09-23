88b62a9bfc87ec8ffaba8b20cca0b69d84930b7c
Desi <dimova@gmail.com>
2026-09-23 14:25:29 +0000

    write file

--- Start/Ch6/writefile_start.py
+++ Start/Ch6/writefile_start.py
@@ -4,12 +4,15 @@
 
 
 # Open a file for writing and create it if it doesn't exist
-
+# sample_file= open("sample.txt", "w+")
+# sample_file.write("This is a sample line of text.\n")
+# sample_file.close()
 
 # Open the file for appending text to the end
-
+sample_file= open("sample.txt", "a+")
 
 # write some lines of data to the file
-
+sample_file.write("This is another line of text.\n")
+sample_file.close()
 
 # close the file when done
